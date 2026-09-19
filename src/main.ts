fe7aa8c378dde77306c3e8898f721758ed310294
TimEtOff <timeogodefroy@gmail.com>
2026-09-19 20:20:45 +0200

    fix: Infinite reload when fetching sound with no image

--- src/main.ts
+++ src/main.ts
@@ -105,7 +105,7 @@ export default class ModuleInstance extends InstanceBase<ModuleSchema> {
 					if (data.length == 2) {
 						if (data[1].startsWith('data:')) {
 							this.setSoundImage(data[0], data[1])
-						} else {
+						} else if (data[1].length != 0) {
 							var encoded = readFileSync(data[1], 'base64')
 							encoded = 'data:' + lookup(data[1]) + ';base64,' + encoded
 							this.setSoundImage(data[0], encoded)
